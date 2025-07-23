c5caad15c7c99e0b55d8c602d857a314e2a1c13b
_ SRZ_ <leosarrazin07@gmail.com>
2025-07-23 04:55:29 +0000

    OK donc la pour l instant je peut pas la publier sur Android il faut que

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,86 @@
+// This is a basic service worker for PWA offline functionality.
 
-// This is a basic service worker.
-// It's currently only used for enabling Push Notifications.
+const CACHE_NAME = 'prepy-cache-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icon.png', // Assuming this is your main icon
+  '/apple-icon.png',
+  '/favicon.ico',
+  // Add other critical assets here, like CSS or JS files if they have static names.
+  // Be careful with what you cache, as it will be stored on the user's device.
+];
 
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || 'PrEPy';
-  const options = {
-    body: data.body || 'Notification de PrEPy',
-    icon: data.icon || '/icon-192x192.png',
-    badge: '/badge-72x72.png',
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  // We only want to cache GET requests.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    clients.openWindow('/')
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
