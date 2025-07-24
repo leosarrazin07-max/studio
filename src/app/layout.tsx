504353224dae8b410ab0188594783d2fa26bf68b
_ SRZ_ <leosarrazin07@gmail.com>
2025-07-24 01:12:09 +0000

    OK gemini on va créé les icône et favicon de l app tu dois les séparé du

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className="font-body antialiased">
         {children}
